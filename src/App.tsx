1f2db758d487db3c541148040c2d624e1bec660a
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-30 18:32:03 +0900

    とりあえず実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
-
 function App() {
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      await fetch(`${window.location.origin}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      alert("File uploaded");
+    }
+  };
+
   return (
     <>
       <h1>Vite + React</h1>
+      <input type="file" onChange={handleUpload} />
     </>
   );
 }
