22ef93ba040a449843cf857732ebc0edbca40753
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-30 19:18:43 +0900

    最低限レイアウト実装

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,16 +3,44 @@ import { css } from "../styled-system/css";
 export const Layout = ({ children }: { children: React.ReactNode }) => (
   <div
     className={css({
+      // 最も外枠
       display: "flex",
       flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      gap: "1rem",
-      bg: "gray-100",
     })}
   >
-    <h1>Layout</h1>
-    {children}
+    <header
+      className={css({
+        // ヘッダー
+        padding: 2,
+        paddingX: 8,
+        bg: "gray.100",
+        gradientFrom: "gray.200",
+        borderBottomWidth: 2,
+        borderRadius: "lg",
+      })}
+    >
+      <h1 className={css({ fontSize: "2xl" })}>Media Knife</h1>
+    </header>
+
+    <main
+      className={css({
+        // メインコンテンツ
+
+        padding: 2,
+      })}
+    >
+      <div
+        className={css({
+          display: "flex",
+          flexDirection: "column",
+          gap: 4,
+          borderRadius: "lg",
+          borderWidth: 2,
+          padding: 4,
+        })}
+      >
+        {children}
+      </div>
+    </main>
   </div>
 );
