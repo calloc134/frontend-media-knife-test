2a62ab7fffa4600dc48afa7221d16c059c10e052
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-30 19:30:47 +0900

    react dropzone導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { css } from "../styled-system/css";
+
 function App() {
-  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  const handleUpload = useCallback(async (acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
 
     if (file) {
       const formData = new FormData();
@@ -12,13 +16,45 @@ function App() {
 
       alert("File uploaded");
     }
-  };
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: handleUpload,
+  });
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <input type="file" onChange={handleUpload} />
-    </>
+    <div
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center",
+        gap: 4,
+      })}
+    >
+      <h1 className={css({ fontSize: "2xl" })}>Media Knife</h1>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // class="icon icon-tabler icon-tabler-upload"
+          className={css({
+            width: 24,
+            height: 24,
+          })}
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="#2c3e50"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+          <path d="M7 9l5 -5l5 5" />
+          <path d="M12 4l0 12" />
+        </svg>
+      </div>
+    </div>
   );
 }
 
