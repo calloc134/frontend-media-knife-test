5fe92820ce6666fec69eea11ebed8fdfc3282e6a
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-31 15:25:35 +0900

    ラジオボタンの追加

--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -2,11 +2,20 @@ import { useSearch } from "@tanstack/react-router";
 import { css } from "../../styled-system/css";
 import { Button } from "~/components/ui/button";
 import { useNavigate } from "@tanstack/react-router";
+import * as RadioGroup from "~/components/ui/radio-group";
 
 export const ProgressPage = () => {
   const param = useSearch({ from: "/progress" });
 
   const navigate = useNavigate();
+
+  const options = [
+    { value: "ffmpeg", label: "形式変換" },
+    { value: "demucs", label: "音源分離" },
+    { value: "esrgan", label: "拡大" },
+    { value: "segmenration", label: "被写体切り抜き" },
+    { value: "whisper", label: "書き起こし" },
+  ];
 
   return (
     <div
@@ -24,7 +33,22 @@ export const ProgressPage = () => {
       <div
         className={css({ display: "flex", gap: 2, flexDirection: "column" })}
       >
-        <Button size={"2xl"}>加工を開始</Button>
+        <form>
+          <RadioGroup.Root defaultValue={"ffmpeg"}>
+            {options.map((option) => (
+              <RadioGroup.Item key={option.value} value={option.value}>
+                <RadioGroup.ItemControl />
+                <RadioGroup.ItemText>{option.label}</RadioGroup.ItemText>
+              </RadioGroup.Item>
+            ))}
+          </RadioGroup.Root>
+
+          <div className={css({ height: 8 })} />
+
+          <Button size={"2xl"} type={"submit"}>
+            加工を開始する
+          </Button>
+        </form>
 
         <Button size={"2xl"} onClick={() => navigate({ to: "/" })}>
           加工をやめる
