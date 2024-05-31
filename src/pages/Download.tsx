abfbe3811608769097e06fd50cbd913c404fe99a
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-31 16:49:06 +0900

    ダウンロード画面簡易実装

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -1,8 +1,32 @@
+import { Button } from "~/components/ui/button";
+import { css } from "../../styled-system/css";
+import { useSearch } from "@tanstack/react-router";
+
 export const DownloadPage = () => {
+  const param = useSearch({ from: "/download" });
+
   return (
-    <div>
-      <h1>Download</h1>
-      <p>Download Page</p>
+    <div
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 2,
+      })}
+    >
+      <h1 className={css({ fontSize: "4xl" })}>Media Knife</h1>
+      <p className={css({ fontSize: "xl" })}>
+        ファイルのダウンロードを行います。
+      </p>
+      <p className={css({ fontSize: "xl" })}>ファイル名: {param.filename}</p>
+      <div className={css({ height: 4 })} />
+
+      <Button
+        size={"2xl"}
+        // onClick={() => window.open(`/download/${param.filename}`)}
+      >
+        ダウンロード
+      </Button>
     </div>
   );
 };
